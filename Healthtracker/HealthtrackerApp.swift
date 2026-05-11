7bb769a2ff91bb131ca9c80b7cf2468fe4609e50
jorrawar1 <jorrawar@umich.edu>
2026-05-11 14:37:26 -0400

    Initial Commit

---
+++ Healthtracker/HealthtrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthtrackerApp.swift
+//  Healthtracker
+//
+//  Created by Jorrawar Grewal on 5/11/26.
+//
+
+import SwiftUI
+
+@main
+struct HealthtrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
